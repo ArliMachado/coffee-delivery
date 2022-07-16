2899ddff54d57ae32a0aadbbfd365d2a25ceffb4
Arli Machado <arli.machado@gmail.com>
2022-07-15 23:28:24 -0300

    Corrigindo layout default

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 export const LayoutContainer = styled.div`
   max-width: 90rem;
   height: 100vh;
-  margin: 0 10rem;
-  /* padding: 0 10rem; */
+  padding: 0 10rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
 `
